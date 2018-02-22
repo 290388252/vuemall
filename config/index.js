e5f6f4781215130272b50e165afb168ee9a608ba
ychao <290388252@qq.com>
2018-02-22 17:04:41 +0800

    [Fix] 4000 port is not allow

--- config/index.js
+++ config/index.js
@@ -6,19 +6,23 @@ const path = require('path')
 
 module.exports = {
   dev: {
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
 
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
       '/goods': {
-        target: 'http://localhost:4000'
+        target: 'http://localhost:5000',
+        // changeOrigin: true,
+        // pathRewrite: {
+        //   '^/goods': ''
+        // }
       },
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
